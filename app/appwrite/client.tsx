51c76f56d9f251e97901e3b8802e60d728874fc5
ejoseph <emz_joseph@yahoo.com>
2026-03-19 13:53:35 -0600

    Implement Routing, Dashboard Layout & UI, Auth Functionality

---
+++ app/appwrite/client.tsx
@@ -0,0 +1,18 @@
+import { Account, Client, Databases } from "appwrite";
+
+export const appwriteConfig = {
+  endpointUrl: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_APPWRITE_API,
+  databaseId: import.meta.env.VITE_APPWRITE_DB,
+};
+
+const client = new Client()
+  .setEndpoint(appwriteConfig.endpointUrl)
+  .setProject(appwriteConfig.projectId);
+
+const account = new Account(client);
+const database = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, database, storage };
